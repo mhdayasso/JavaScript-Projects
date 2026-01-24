2e43ce74654d333f405e8123f3b68c85dbdd92b1
mhdayasso <mhd.ayasso@gmail.com>
2026-01-23 23:07:26 -0500

    Add Basic JavaScript Projects - Project1 requirements

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,24 @@
-console.log("main.js loaded ✅");
-window.alert("Hello! This is window.alert() ✅");
+// Project 1: Basic JavaScript Requirements
+// This file demonstrates window.alert(), document.write(), variables, concatenation,
+// expressions, and comments.
+
+// 1) window.alert() method
+window.alert("Welcome! JavaScript is running.");
+
+// 2) Create two variables (strings)
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// 3) Concatenate Sent1 and Sent2
+var fullSentence = Sent1 + Sent2;
+
+// 4) document.write() method to display the concatenated result
+document.write(fullSentence);
+
+// 5) Write an expression (example: math expression)
+var num1 = 10;
+var num2 = 5;
+var result = num1 * num2; // expression: 10 * 5
+
+// Write the expression result to the page
+document.write("<br>Expression result (10 * 5) = " + result);
