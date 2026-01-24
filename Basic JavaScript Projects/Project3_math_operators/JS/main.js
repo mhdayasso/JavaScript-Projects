0898275b8c5359c8b54161d8185bcca4455a6f61
mhdayasso <mhd.ayasso@gmail.com>
2026-01-23 23:25:35 -0500

    Add Project3 math operators

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -21,3 +21,20 @@ function divisionFunction() {
   var result = 10 / 5;
   document.getElementById("MathDiv").textContent = "10 / 5 = " + result;
 }
+
+// Multiple mathematical operations displayed at once
+function multipleOperations() {
+  var a = 20;
+  var b = 4;
+
+  var add = a + b;
+  var sub = a - b;
+  var mul = a * b;
+  var div = a / b;
+
+  document.getElementById("MathAll").innerHTML =
+    a + " + " + b + " = " + add + "<br>" +
+    a + " - " + b + " = " + sub + "<br>" +
+    a + " * " + b + " = " + mul + "<br>" +
+    a + " / " + b + " = " + div;
+}
