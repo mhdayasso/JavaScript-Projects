c41598c647b2f65430602d3fa588a97a9cb2d777
mhdayasso <mhd.ayasso@gmail.com>
2026-01-23 23:17:40 -0500

    Add Project3 math operators

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,7 @@
+// Project 3: Math Operators
+// This function performs an addition operation and displays the result in the HTML paragraph.
+
+function additionFunction() {
+  var result = 10 + 5; // addition operation
+  document.getElementById("Math").textContent = "10 + 5 = " + result;
+}
