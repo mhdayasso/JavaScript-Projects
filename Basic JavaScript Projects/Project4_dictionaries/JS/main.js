8cd81b76160f431ed79875fd6f98bbd95ba5a5eb
mhdayasso <mhd.ayasso@gmail.com>
2026-01-23 23:51:32 -0500

    Complete Project4 dictionaries with delete operator

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,16 +1,23 @@
-// Project 4: Dictionaries (JavaScript Objects)
-// A "dictionary" in JavaScript is commonly represented using an object with key-value pairs.
+// Project 4: Dictionaries Assignment
+// A "dictionary" in JavaScript can be represented using an object with key-value pairs.
 
 var car = {
   make: "Toyota",
   model: "Corolla",
   year: 2020,
-  color: "White",
-  fuelType: "Gasoline"
+  color: "White"
 };
 
-// This function outputs a chosen dictionary value into the HTML element with id="Dictionary"
+// This function runs when the user clicks the paragraph in index.html
 function showDictionaryValue() {
+  // Choose a key we plan to display
+  var keyToDelete = "model";
+
+  // Delete the key from the dictionary BEFORE displaying it
+  delete car[keyToDelete];
+
+  // Output the key-value pair into the HTML element
+  // Since the key was deleted, the value will be undefined
   document.getElementById("Dictionary").innerHTML =
-    "Car model: " + car.model;
+    keyToDelete + ": " + car[keyToDelete];
 }
